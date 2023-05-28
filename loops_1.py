974653f2701412bd3351390bacdddbbb40a51546
Arias54G <valentina.parraa@ecci.edu.co>
2023-05-28 12:03:40 -0500

    Se carga programa que genera un looop

---
+++ loops_1.py
@@ -0,0 +1,16 @@
+
+print('==============================================')
+print('==           Realizar un patrón             ==')
+print('==============================================')
+
+
+"""
+Éste programa realiza un patrón
+
+"""
+
+
+valor=("*","**","*","****")
+
+for i in valor:
+  print(i)
